361d4b87bddfb73cfc7efd91dc12454ca742d3aa
KiamMota <somentekiam@gmail.com>
2026-04-02 00:54:37 -0300

    adding for version 0.12

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,15 @@
+-- lua/plugins/colorscheme.lua
+
 vim.pack.add({
-  "https://github.com/mofiqul/vscode.nvim"
+  'https://github.com/mofiqul/vscode.nvim',           -- VS Code dark
+  "https://github.com/mofiqul/dracula.nvim",
 })
+
+-- Troque o nome para experimentar cada um:
+-- 'vscode'       → VS Code dark
+-- 'darcula'      → JetBrains Darcula
+-- 'kanagawa'     → Kanagawa
+-- 'tokyonight'   → Tokyo Night
+-- 'catppuccin'   → Catppuccin Mocha
+
+vim.cmd.colorscheme('vscode')
