1561b5309b7ed09f6bd7ade67b6211f48e21b976
KiamMota <somentekiam@gmail.com>
2026-04-02 21:24:00 -0300

    adicionando fluxo melhor para o nvim tree <leader>cd coloca o cwd do neovim omo padrão

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -16,3 +16,20 @@ require("nvim-tree").setup({
 vim.keymap.set("n", "<leader>e", function()
   require("nvim-tree.api").tree.toggle()
 end, { noremap = true, silent = true })
+
+local function set_tree_cwd()
+  local api = require("nvim-tree.api")
+  local node = api.tree.get_node_under_cursor()
+  
+  if node and node.absolute_path then
+    if vim.fn.isdirectory(node.absolute_path) == 1 then
+      vim.cmd("cd " .. node.absolute_path)
+      print("cwd changed to: " .. node.absolute_path)
+    else
+      print("Selecione uma pasta, não um arquivo")
+    end
+  end
+end
+
+-- Map para nvim-tree (quando a árvore está aberta)
+vim.keymap.set("n", "<leader>cd", set_tree_cwd, { noremap = true, silent = true })
